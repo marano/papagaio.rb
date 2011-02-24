76fd183d42c50156dc383c79b06697d8d32b2257
marano <thiagomarano@gmail.com>
2011-02-24 11:21:02 -0300

    Will pick a line right after start running.

--- papagaio.rb
+++ papagaio.rb
@@ -17,7 +17,7 @@ end
 
 scheduler = Rufus::Scheduler.start_new
 
-scheduler.every '5m' do
+scheduler.every '5m', :first_in => '1s' do
   lines = File.readlines 'lines.txt'
   lines.empty? and return
   say lines[rand(lines.size)]
